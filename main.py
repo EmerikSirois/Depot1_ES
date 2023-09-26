44201a16a997e98d71826ca755cb10061cd0e7f5
1er commit <2293583@cegepoutaouais.qc.ca>
2023-09-26 14:36:15 -0400

    3e commit : Ajout d'une fonction et d'explication additionnelle

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 print("Bonjour...")
 print("Je continue à modifier mon code")
+# Portée des variable
+#new*
+def CalculerDouble(nombre):
+
+    # Docstring : Définir ma fonction
+    """
+    Calcule le double d'un nombre
+    :param nombre:
+    :return: le double du nombre entré
+    """
+    # La portée de la variable « nombre » est limité à la fonction ou elle se trouve
+    print(nombre)
+    return nombre * 2
+
+# Programme principal
+# La variable « nombre_2 » à une portée globale (Elle est reconnu partout)
+nombre_2 = 55
+nombre = 60
+resultat = CalculerDouble(2)
+
+print("Résultat ", resultat)
